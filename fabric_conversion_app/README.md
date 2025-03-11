7ecf78aad4f3f92b3814262ea613cc8968c200db
Fahad-warsi <fahad7swarsi@gmail.com>
2025-03-11 23:26:41 +0500

    Added README.md file

---
+++ fabric_conversion_app/README.md
@@ -0,0 +1,31 @@
+# Fabric Conversion App
+
+A Streamlit-based web application for converting fabric measurements between meters and kilograms. This app includes user authentication and a dashboard for performing fabric conversions.
+
+## Features
+- **User Authentication**: Secure login system with username and password.
+- **Fabric MTR to KGS Conversion**: Convert fabric length in meters to weight in kilograms.
+- **Fabric KGS to MTR Conversion**: Convert fabric weight in kilograms to length in meters.
+- **Responsive Design**: Works seamlessly on desktop and mobile devices.
+
+## Screenshots
+![Login Page](screenshots/login.png)
+![Dashboard](screenshots/dashboard.png)
+
+## How to Run
+Follow these steps to run the app locally:
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/Fahad-warsi/Python-project.git
+
+2. **Navigate to the project folder:**
+    ```bash
+    cd fabric-conversion-app
+
+3. **Run the app:**
+    ```bash
+    streamlit run login.py
+4. **Access the app:**
+    Open your browser and go to http://localhost:8501.
+
--- fabric_conversion_app/database/db.py
+++ fabric_conversion_app/database/db.py
@@ -1,24 +1,24 @@
 import sqlite3
 
-# try:
-#     with sqlite3.connect("auth.db") as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             username TEXT NOT NULL,
-#             password TEXT NOT NULL
-#             )""")
-#         print("Created table successfully")
-# except:
-#     conn.rollback()
-#     print("Transaction Failed...")
-# finally:
-#     conn.close()
+try:
+    with sqlite3.connect("auth.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+            )""")
+        print("Created table successfully")
+except:
+    conn.rollback()
+    print("Transaction Failed...")
+finally:
+    conn.close()
 
 #TESTING.....................
-with sqlite3.connect("auth.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+# with sqlite3.connect("auth.db") as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT * FROM users")
+#         rows = cursor.fetchall()
+#         for row in rows:
+#             print(row)
